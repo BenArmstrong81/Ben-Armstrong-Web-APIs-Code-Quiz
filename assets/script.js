b0db6818a163b789668e6aab731edb008abda4d9
Ben Armstrong <benarmstrong@live.com>
2023-01-05 00:20:20 +1030

    Styled CSS sheet and added background image

--- assets/script.js
+++ assets/script.js
@@ -2,95 +2,49 @@
 var questions = [
     {
         questions: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
-        choices: [
-            "if (i != 5)",
-            "if (i <> 5)",
-            "if i =! 5 then",
-            "if i <> 5"
-        ],
+        choices: ["if (i != 5)", "if (i <> 5)", "if i =! 5 then", "if i <> 5"],
         answer: "if (i != 5)"
     },
     {
         questions: "Commonly used data types DO NOT include:",
-        choices: [
-            "strings",
-            "booleans",
-            "alerts",
-            "numbers"
-        ],
+        choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
     },
     {
         questions: "Which one of these is not among the three different types of errors in JavaScript?",
-        choices: [
-            "Animation time errors",
-            "Load time errors",
-            "Run time errors",
-            "Logical Errors"
-        ],
+        choices: ["Animation time errors", "Load time errors", "Run time errors", "Logical Errors"],
         answer: "Animation time errors"
     },
     {
         questions: "Inside which HTML element do we put the JavaScript?",
-        choices: [
-            "<script>",
-            "<js>",
-            "<javascript>",
-            "<JS>",
-            "All of the above"
-        ],
+        choices: ["<script>", "<js>", "<javascript>", "<JS>", "All of the above"],
         answer: "<script>"
     },
     {
         questions: "The condition in an if / else statement is enclosed within ____.",
-        choices: [
-            "quotes",
-            "curly brackets",
-            "parentheses",
-            "square brackets"
-        ],
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
     },
     {
         questions: "Arrays in JavaScript can be used to store ____.",
-        choices: [
-            "lnumbers and strings",
-            "other arrays",
-            "booleans",
-            "al of the above"
-        ],
+        choices: ["lnumbers and strings", "other arrays", "booleans", "al of the above"],
         answer: "all of the above"
     },
     {
         questions:
             "String values must be enclosed within ____ when being assigned to variables.",
-        choices: [
-            "commas",
-            "curly brackets",
-            "quotes",
-            "parentheses"
-        ],
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
         answer: "quotes"
     },
     {
         questions:
             "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: [
-            "JavaScript",
-            "terminal / bash",
-            "for loops",
-            "console.log"
-        ],
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
         answer: "console.log"
     },
     {
         questions: "What is the type of Pop up boxes available in JavaScript?:",
-        choices: [
-            "Alert",
-            "Confirm",
-            "Prompt",
-            "All the above"
-        ],
+        choices: ["Alert", "Confirm", "Prompt", "All the above"],
         answer: "All the above"
     }
 ];
